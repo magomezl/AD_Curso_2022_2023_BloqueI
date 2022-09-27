c1b533218bf4b07419ce2748e1ee624fea6f36d6
admin <admin@DPTO2.INFOR.JM>
2022-09-27 14:35:01 +0200

    Ejercicio Pendiente de revisar

--- src/Ejercicio1_3.java
+++ src/Ejercicio1_3.java
@@ -1,10 +1,14 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+// https://github.com/magomezl/AD_Curso_2022_2023_BloqueI.git
 
 public class Ejercicio1_3 {
 	
+	private static ArrayList<Integer> cuentaNivel = new ArrayList<Integer>();
+	private static ArrayList<Integer> cuentaElementos = new ArrayList<Integer>();
 	private static int nivel = 0;
-	private static int cuentaElementos = 0;
 	
 	public static void ejercicio1_2() {
 		File dirActual = new File(".");
@@ -23,10 +27,10 @@ public class Ejercicio1_3 {
 	    String nombre = "."+ System.getProperty("file.separator") + scn.nextLine (); 
 	    File f = new File(nombre);
 	    if (f.exists()) {
+	    	
 	    	 if (f.isFile()) {
 	    		 trataFichero(f);
 	    	 }else {
-	    		 
 	    		 trataDirectorio(f);
 	    	 }
 	    }else {
@@ -37,26 +41,42 @@ public class Ejercicio1_3 {
 	public static void trataDirectorio(File f) {
 		
 		File[] contenido = f.listFiles();
-		cuentaElementos = contenido.length;
-		++nivel;
+		int elementos = contenido.length;
+		cuentaElementos.add(nivel, contenido.length);
+		cuentaNivel.add(nivel, nivel);
+		
 		StringBuilder cad = new StringBuilder();
-		for (int i=0; i<=nivel; i++)
-			cad.append("\t");
+		
+		if ( cuentaElementos.get(nivel)>0) {
+			//System.out.println("cuentaElementos.get(nivel) " + cuentaElementos.get(nivel));
+			for (int i=0; i<cuentaNivel.get(nivel); i++) {
+					//System.out.println("un tabulador");
+					cad.append("\t");
+			}
+		}
+		
 		System.out.println(cad.append("d\t" + f.getName() + "\t\t" + contenido.length + " elementos"));
 		for (File itemFile: contenido) {
+			
 			 if (itemFile.isFile()) {
 	    		 trataFichero(itemFile);
+	    		 cuentaElementos.add(nivel, --elementos);
 	    	 }else {
+	    		 ++nivel;
+	    		 cuentaElementos.add(nivel, --elementos);
+	    		 
 	    		 trataDirectorio(itemFile);
 	    	 }
-    		 --cuentaElementos;
 		}
 	}
 
 	public static void trataFichero(File f) {
 		StringBuilder cad = new StringBuilder();
-		for (int i=0; i<=nivel; i++)
-			cad.append("\t");
+		if ( cuentaElementos.get(nivel)>0) {
+			for (int i=0; i<cuentaNivel.get(nivel); i++) {
+					cad.append("\t");
+			}
+		}
 		System.out.println(cad.append("f\t" + f.getName() + "\t(" + f.length() + "bytes)\t" + (f.canRead()?"r":"-") + (f.canWrite()?"w":"-") + (f.canExecute()?"x":"-")));
 	}
 	
